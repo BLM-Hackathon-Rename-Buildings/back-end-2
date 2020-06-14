c9ccdb244a25ecd1b56c92f6237ad50a80684c79
Natalie Jane Edson <nataliejaneedson@gmail.com>
2020-06-14 11:08:27 -0700

    adjusts admin

--- symbols/admin.py
+++ symbols/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 
 from .models import *
 
+
+class SymbolAdmin(admin.ModelAdmin):
+    list_display = ('name','city','state',)
+    list_filter = ('approved',)
+
 admin.site.register(Contact)
 admin.site.register(Honoree)
-admin.site.register(Symbol)
+admin.site.register(Symbol, SymbolAdmin)
