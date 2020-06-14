fa6b4b2c2fb72f2383ab6b40b6a30ca20cd1125a
Natalie Jane Edson <nataliejaneedson@gmail.com>
2020-06-13 17:08:46 -0700

    adjusts honoree model

--- symbols/models.py
+++ symbols/models.py
@@ -25,7 +25,7 @@ SYMBOL_TYPES = (
 )
 
 class Honoree(models.Model): 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     photo = models.ImageField(null=True)
 
